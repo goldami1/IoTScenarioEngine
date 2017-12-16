03d7beb110ffe015efe9294df0c2c5d77dc1fe01
Gil Ronen <gilgilronen@gmail.com>
2017-12-16 03:20:42 +0200

    clear the unnecessary comments

--- src/db/DBHandler.java
+++ src/db/DBHandler.java
@@ -58,159 +58,56 @@ public class DBHandler implements IDBHandler
 	
 
 	
-	
-	
-	public static boolean isUsernameAvailable(String i_userName) {
+	public static boolean isUsernameAvailable(String i_userName) 
+	{
 		// TODO Auto-generated method stub
 		return false;
 		
+
+	}
+
+	
+	public void addCustomer(String i_firstName, String i_lastName, String i_userName, String i_userPassword, String i_email) 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	public void addVendor(String i_vendorName, String i_userName, String i_userPassword, String i_email, String i_vendorDescription, String i_vendorLogoPic) 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	public boolean customerConnectionAuthentication(String i_CustomerUsername, String i_CustomerPassword) 
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
+	
+	public boolean vendorConnectionAuthentication(String i_VendorUsername, String i_VendorPassword) 
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
+	
+	public boolean isExistVendor(String i_VendorUsername) 
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
+	
+	public boolean isExistCustomer(String i_CustomerUsername) 
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
 }
-
-
-	
-	public void addCustomer(String i_firstName, String i_lastName, String i_userName, String i_userPassword,
-			String i_email) {
-		// TODO Auto-generated method stub
-		
-	}
-
-
-	
-	public void addVendor(String i_vendorName, String i_userName, String i_userPassword, String i_email,
-			String i_vendorDescription, String i_vendorLogoPic) {
-		// TODO Auto-generated method stub
-		
-	}
-
-
-
-	public boolean customerConnectionAuthentication(String i_CustomerUsername, String i_CustomerPassword) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-
-	
-	public boolean vendorConnectionAuthentication(String i_VendorUsername, String i_VendorPassword) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-
-	
-	public boolean isExistVendor(String i_VendorUsername) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-
-	
-	public boolean isExistCustomer(String i_CustomerUsername) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
-
-import utils.Constants;
-import engine.User;
-import engine.Vendor;
-import engine.Action;
-import engine.Customer;
-import engine.Event;
-import engine.Range;
-import engine.Scenario;
-
-
-
-
-
-
-
-
-
-
-public class DBHandler implements IDBHandler 
-{
-	private ConnectionPool pool = ConnectionPool.getInstance();
-	
-	public DBHandler()
-	{
-		super();
-	}	
-	
-	
-	
-
-	public void addCustomer(String i_firstName, String i_lastName, String i_userName, String i_userPassword, String i_email)
-	{ 
-		
-	}
-	
-	public void addVendor(String i_vendorName, String i_userName, String i_userPassword, String i_email, String i_vendorDescription, String i_vendorLogoPic)
-	{ 
-		
-	}
-	
-	public boolean vendorConnectionAuthentication(String i_VendorUsername, String i_VendorPassword)
-	{
-		return true;
-	}
-	
-	public boolean isExistVendor(String i_VendorUsername)
-	{
-		return true;
-	}
-	
-	public boolean isExistCustomer(String i_CustomerUsername)
-	{
-		return true;
-	}
-	
-	
-	
-	
-	
-
-}*/
