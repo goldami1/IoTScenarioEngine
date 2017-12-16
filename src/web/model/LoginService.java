fef045f609675e707b991bef49d3c3967d4737ac
Roma Dee <roma.dyment@gmail.com>
2017-12-16 19:09:14 +0200

    sign in authentication added

--- src/web/model/LoginService.java
+++ src/web/model/LoginService.java
@@ -1,9 +1,20 @@
 package web.model;
 
+import db.DBHandler;
+
 public class LoginService {
+
 	public boolean authenticate(String username, String password) {
-		//TODO:
-		// call DAO authenticate
-			return true;
+			DBHandler db = DBHandler.getInstance();
+			
+			boolean isOk = false;
+			try {
+				isOk = db.userConnectionAuth(username, password);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			return isOk;
 	}
 }
