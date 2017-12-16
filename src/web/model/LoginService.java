7f8ab4f86189db7d8793de1f7bd4773e549de3b1
Roma Dee <roma.dyment@gmail.com>
2017-12-16 19:09:33 +0200

    login authentication added

--- src/web/model/LoginService.java
+++ src/web/model/LoginService.java
@@ -5,13 +5,15 @@ import db.DBHandler;
 public class LoginService {
 
 	public boolean authenticate(String username, String password) {
+		
 			DBHandler db = DBHandler.getInstance();
 			
 			boolean isOk = false;
+			
 			try {
 				isOk = db.userConnectionAuth(username, password);
 			} catch (Exception e) {
-				// TODO Auto-generated catch block
+
 				e.printStackTrace();
 			}
 			
