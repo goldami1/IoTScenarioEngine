8727fc93f9ba7d19d5b0680e4c8a9b2f2b06a7b5
goldami1 <goldami1@gmail.com>
2017-12-18 17:52:08 +0200

    update db 2

--- src/db/IDBHandler.java
+++ src/db/IDBHandler.java
@@ -1,5 +1,6 @@
 package db;
 
+import java.sql.SQLException;
 import java.util.LinkedList;
 
 import engine.Customer;
@@ -10,10 +11,10 @@ import engine.User;
 import engine.Vendor;
 
 public interface IDBHandler {
-	//public boolean userConnectionAuth(User i_User) throws Exception;
-	public boolean userConnectionAuth(String i_username, String i_password) throws Exception;
-	public boolean addCustomer(String i_firstName, String i_lastName, String i_userName, String i_userPassword,String i_email);
-	public boolean addVendor(String i_vendorName, String i_userName, String i_userPassword, String i_email, String i_vendorDescription, String i_vendorLogoPic);
+	public boolean userConnectionAuth(String i_Username, String i_UserPassword) throws Exception;
+	public boolean addCustomer(String i_firstName, String i_lastName, String i_userName, String i_userPassword, String i_email) throws Exception;
+	public boolean addVendor(String i_vendorName, String i_userName, String i_userPassword, String i_email, String i_vendorDescription, String i_vendorLogoPic) throws SQLException;
+	public LinkedList<String> selectQ(String i_selectQ) throws Exception;
 	public boolean addDevice(Device i_dev);
 	public boolean addScenario(Scenario i_Scenario);
 	public LinkedList<Scenario> getScenariosByEvent(Event i_event);
