b8b5430475f2470105c97a773d40438d9d968710
Roman Dyment <roma.dyment@gmail.com>
2018-01-21 17:14:55 +0200

    scenario add actionevent form added

---
+++ client/src/components/scenarios/add/CreateModal.js
@@ -0,0 +1,209 @@
+import React, { Component } from "react";
+import { Link, withRouter } from "react-router-dom";
+import ContentWrapper from "../../common/ContentWrapper";
+import {
+	DatePicker,
+	Select,
+	Checkbox,
+	TimePicker,
+	Form,
+	Cascader,
+	Modal,
+	Radio,
+	Button,
+	Icon,
+	Row,
+	Col,
+	Layout
+} from "antd";
+
+const { Header, Footer, Sider, Content } = Layout;
+const CheckboxGroup = Checkbox.Group;
+const RadioButton = Radio.Button;
+const RadioGroup = Radio.Group;
+const Option = Select.Option;
+const FormItem = Form.Item;
+
+const formItemLayout = {
+	labelCol: {
+		xs: { span: 24 },
+		sm: { span: 5 }
+	},
+	wrapperCol: {
+		xs: { span: 24 },
+		sm: { span: 16 }
+	}
+};
+
+class CreateModal extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			options: [
+				{
+					value: "clock",
+					label: "clock",
+					children: [
+						{
+							value: "alram clock",
+							label: "alram clock"
+						},
+						{
+							value: "play music",
+							label: "play music"
+						},
+						{
+							value: "say time",
+							label: "say time"
+						}
+					]
+				},
+				{
+					value: "lamp",
+					label: "lamp",
+					children: [
+						{
+							value: "turn light",
+							label: "turn light"
+						},
+						{
+							value: "change color to black",
+							label: "change color to black"
+						},
+						{
+							value: "change color to red",
+							label: "change color to red"
+						},
+						{
+							value: "change color to pink",
+							label: "change color to pink"
+						}
+					]
+				}
+			],
+			triggerTypeOptions: [
+				{
+					value: "device",
+					label: "Device"
+				},
+				{
+					value: "time",
+					label: "Time",
+					children: [
+						{
+							value: "daily",
+							label: "Daily"
+						},
+						{
+							value: "date",
+							label: "Specific date"
+						}
+					]
+				}
+			],
+			trigger: ["device"]
+		};
+	}
+
+	onChange = event => {
+		this.setState({ [event.target.name]: event.target.value });
+	};
+
+	onTriggerChange = (values, selectedOptions) => {
+		this.setState({ trigger: values });
+	};
+
+	onTimeChange = time => {
+		console.log(time);
+		this.setState({ time: time });
+	};
+
+	onDateChange = date => {
+		console.log(date);
+		this.setState({ date: date });
+	};
+
+	onDayChange = day => {
+		console.log(day);
+		this.setState({ day: day });
+	};
+
+	timeForm = () => {
+		const days = [
+			"Sunday",
+			"Monday",
+			"Tuesday",
+			"Wednesday",
+			"Thursday",
+			"Friday",
+			"Saturday"
+		];
+		const children = [];
+		for (let i = 1; i < 8; i++) {
+			children.push(<Option key={i}>{days[i - 1]}</Option>);
+		}
+		if (this.state.trigger[1] == "date")
+			return (
+				<FormItem label="Date and Time" {...formItemLayout}>
+					<DatePicker
+						value={this.state.date}
+						onChange={this.onDateChange}
+						showToday={false}
+						showTime
+						format="DD/MMM/YY HH:mm"
+						placeholder="Select Date and Time"
+					/>
+				</FormItem>
+			);
+		else
+			return (
+				<Form>
+					<FormItem label="Time" {...formItemLayout}>
+						<TimePicker
+							value={this.state.time}
+							onChange={this.onTimeChange}
+							format="HH:mm"
+						/>
+					</FormItem>
+					<FormItem label="Day" {...formItemLayout}>
+						<Select
+							mode="tags"
+							value={this.state.day}
+							onChange={this.onDayChange}
+							defaultValue={["1", "2", "3", "4", "5", "6", "7"]}
+						>
+							{children}
+						</Select>
+					</FormItem>
+				</Form>
+			);
+	};
+	render() {
+		const deviceForm = (
+			<FormItem label={this.props.type} {...formItemLayout}>
+				<Cascader options={this.state.options} />
+			</FormItem>
+		);
+		return (
+			<Modal
+				visible={this.props.visible}
+				title={`Create a new ${this.props.type}`}
+				okText="Create"
+				onCancel={this.props.onCancel}
+				onOk={this.props.onOk}
+			>
+				<FormItem label="Trigger type" {...formItemLayout}>
+					<Cascader
+						size="large"
+						value={this.state.trigger}
+						options={this.state.triggerTypeOptions}
+						onChange={this.onTriggerChange}
+					/>
+				</FormItem>
+				{this.state.trigger[0] == "time" ? this.timeForm() : deviceForm}
+			</Modal>
+		);
+	}
+}
+
+export default withRouter(CreateModal);
--- client/src/components/scenarios/add/index.js
+++ client/src/components/scenarios/add/index.js
@@ -1,37 +1,92 @@
 import React, { Component } from "react";
 import { Link, withRouter } from "react-router-dom";
-import ContentWrapper from '../../common/ContentWrapper';
-import { Button, Icon,Row, Col,Layout } from 'antd';
-const { Header, Footer, Sider, Content } = Layout;
+import ContentWrapper from "../../common/ContentWrapper";
+import CreateModal from "./CreateModal";
+import {
+	Form,
+	Cascader,
+	Modal,
+	Radio,
+	Button,
+	Icon,
+	Row,
+	Col,
+	Layout
+} from "antd";
 
-<Row>
-	<Col span={12}>col-12</Col>
-	<Col span={12}>col-12</Col>
-</Row>
+const RadioButton = Radio.Button;
+const RadioGroup = Radio.Group;
+const { Header, Footer, Sider, Content } = Layout;
+const FormItem = Form.Item;
 
 class ScenarioForm extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-
+			inventory: "action",
+			modalVisible: false
 		};
 	}
 
-	render() {
-		return (		
-			<Layout style={{margin:'40px', borderRadius:'5px solid '}}>
-				<Content style={{ background: '#fff', padding: '10px' }} >Content</Content>
-				<Sider width={220} style={{color:'white', padding:'10px',background: 'white',borderLeft:'1px solid #ebedf0'}}>
-					<Row>
-						<Button.Group >
-							<Button type="default" >Events</Button>
-							<Button type="default" >Actions</Button>
-							<Button><Icon type="plus" /></Button>
-						</Button.Group>
-					</Row>
-					<Row>
+	addAE = type => () => {
+		console.log(type);
+	};
 
+	onChange = event => {
+		var newState = event.target.value
+			? { [event.target.name]: event.target.value }
+			: { modalVisible: true };
+		this.setState(newState);
+	};
+
+	onModalCancel = () => {
+		this.setState({ modalVisible: false });
+	};
+
+	render() {
+		const formItemLayout = {
+			labelCol: {
+				xs: { span: 24 },
+				sm: { span: 3 }
+			},
+			wrapperCol: {
+				xs: { span: 24 },
+				sm: { span: 16 }
+			}
+		};
+		return (
+			<Layout style={{ margin: "40px", borderRadius: "5px solid " }}>
+				<Content style={{ background: "#fff", padding: "10px" }}>
+					Content
+				</Content>
+				<Sider
+					width={220}
+					style={{
+						padding: "10px",
+						background: "white",
+						borderLeft: "1px solid #ebedf0"
+					}}
+				>
+					<Row>
+						<RadioGroup
+							onChange={this.onChange}
+							name="inventory"
+							value={this.state.inventory}
+						>
+							<RadioButton value="action">Actions</RadioButton>
+							<RadioButton value="event">Events</RadioButton>
+							<RadioButton>
+								<Icon type="plus" />
+							</RadioButton>
+						</RadioGroup>
+						<CreateModal
+							type={this.state.inventory}
+							visible={this.state.modalVisible}
+							onCancel={this.onModalCancel}
+							onOk={this.onModalCancel}
+						/>
 					</Row>
+					<Row />
 				</Sider>
 			</Layout>
 		);
