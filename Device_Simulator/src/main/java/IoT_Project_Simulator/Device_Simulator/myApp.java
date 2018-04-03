54f38ee67e4176a62066e77505d52125d90dfe54
galrotenberg3 <galrotenberg3@gmail.com>
2018-04-03 19:26:43 +0300

    Add simulaotr to project

---
+++ Device_Simulator/src/main/java/IoT_Project_Simulator/Device_Simulator/myApp.java
@@ -0,0 +1,10 @@
+package IoT_Project_Simulator.Device_Simulator;
+
+import javax.ws.rs.Path;
+
+import com.sun.research.ws.wadl.Application;
+
+@Path("webapi")
+public class myApp extends Application{
+
+}
